0037a93c95d3ea7ebe184ff3efc808a958b2c33f
Jamaal <jsedayao@gmail.com>
2015-07-27 19:35:20 -0400

    first commit

---
+++ Pokemon/Pokemon/PKPokedexTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PKPokedexTableViewController.h
+//  Pokemon
+//
+//  Created by Jamaal on 7/27/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PKPokedexTableViewController : UITableViewController
+
+@end
---
+++ Pokemon/Pokemon/PKPokedexTableViewController.m
@@ -0,0 +1,41 @@
+//
+//  PKPokedexTableViewController.m
+//  Pokemon
+//
+//  Created by Jamaal on 7/27/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import "PKPokedexTableViewController.h"
+
+@interface PKPokedexTableViewController ()
+
+@end
+
+@implementation PKPokedexTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+@end
