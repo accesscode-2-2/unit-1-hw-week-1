35e1ca848365188a7bb1bce07bd74bcf6ab4d3e2
shena yoshida <sheyoshida@gmail.com>
2015-07-29 19:02:39 -0400

    added arrays

--- Pokemon/Pokemon/PKTableView.h
+++ Pokemon/Pokemon/PKTableView.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  PKTableView.h
 //  Pokemon
 //
-//  Created by Michael Kavouras on 7/26/15.
+//  Created by Shena Yoshida on 7/27/15.
 //  Copyright (c) 2015 Mike Kavouras. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface PKTableView : UITableViewController
 
 @end
-
