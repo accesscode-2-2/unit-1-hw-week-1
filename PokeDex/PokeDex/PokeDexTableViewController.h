a0b025cc03ebdde8d08e4ceaa4ac7b6285a360a6
elberdev <elberdev@gmail.com>
2015-07-29 07:46:47 -0400

    added navigation controller and custom tableview controller class

--- PokeDex/PokeDex/PokeDexTableViewController.h
+++ PokeDex/PokeDex/PokeDexTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  PokeDexTableViewController.h
 //  PokeDex
 //
 //  Created by Elber Carneiro on 7/29/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface PokeDexTableViewController : UITableViewController
 
 @end
-
