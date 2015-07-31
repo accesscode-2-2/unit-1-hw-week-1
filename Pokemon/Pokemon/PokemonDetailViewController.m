d1ec4552ded014e300d559ac9397a2a517160cfb
Charles Kang <ckangkitchen@gmail.com>
2015-07-30 22:04:14 -0400

    finished requirements doing bonus

---
+++ Pokemon/Pokemon/PokemonDetailViewController.m
@@ -0,0 +1,28 @@
+//
+//  PokemonDetailViewController.m
+//  Pokemon
+//
+//  Created by Charles Kang on 7/28/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import "PokemonDetailViewController.h"
+
+@interface PokemonDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *pokemonNames;
+@property (weak, nonatomic) IBOutlet UIImageView *pokemonImage;
+
+@end
+
+@implementation PokemonDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+//    self.pokemonNames.text = self.pokemonName;
+//    self.pokemonImage.image = [UIImage imageNamed:self.pokemonName];
+    // Do any additional setup after loading the view.
+}
+
+@end
