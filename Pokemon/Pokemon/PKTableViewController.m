05125bb3ff441d1100ed64e6d3da88b8035baa94
Daniel Distant <daniel.distant1@gmail.com>
2015-07-31 13:09:53 -0400

    Changed PKTableView background color to match cell border color, added NSDictionary *pokemonTypes

--- Pokemon/Pokemon/PKTableViewController.m
+++ Pokemon/Pokemon/PKTableViewController.m
@@ -12,6 +12,7 @@
 @interface PKTableViewController ()
 
 @property (nonatomic) NSArray *pokemonData;
+@property (nonatomic) NSDictionary *pokemonTypes;
 
 @end
 
@@ -19,6 +20,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIColor *backgroundColor = [UIColor colorWithRed:95/255.0 green:155/255.0 blue:217/255.0 alpha:1];
+    self.tableView.backgroundView = [[UIView alloc]initWithFrame:self.tableView.bounds];
+    self.tableView.backgroundView.backgroundColor = backgroundColor;
     
     self.pokemonData = @[
                          @"Bulbasaur",
@@ -173,6 +178,294 @@
                          @"Mewtwo",
                          @"Mew",
                          ];
+    
+    //pokemon by type
+    
+    NSArray *bug = @[
+                     @"beedrill",
+                     @"butterfree",
+                     @"caterpie",
+                     @"kakuna",
+                     @"metapod",
+                     @"paras",
+                     @"parasect",
+                     @"pinsir",
+                     @"scyther",
+                     @"venomoth",
+                     @"venonat",
+                     @"weedle"
+                     ];
+    
+    NSArray *dragon = @[
+                        @"dragonair",
+                        @"dratini",
+                        @"dragonite"
+                        
+                        ];
+    
+    NSArray* electric = @[
+                          @"electabuzz",
+                          @"electrode",
+                          @"jolteon",
+                          @"magnemite",
+                          @"magneton",
+                          @"pikachu",
+                          @"raichu",
+                          @"voltorb",
+                          @"zapdos"
+                          
+                          ];
+    
+    NSArray* fire = @[
+                      @"arcanine",
+                      @"charizard",
+                      @"charmander",
+                      @"charmeleon",
+                      @"flareon",
+                      @"growlithe",
+                      @"magmar",
+                      @"moltres",
+                      @"ninetales",
+                      @"ponyta",
+                      @"rapidash",
+                      @"vulpix"
+                      ];
+    
+    NSArray *fighting = @[
+                          @"hitmonchan",
+                          @"hitmonlee",
+                          @"machamp",
+                          @"machoke",
+                          @"machop",
+                          @"mankey",
+                          @"poliwrath",
+                          @"primeape"
+                          
+                          ];
+    
+    NSArray *flying = @[
+                        @"aerodactyl",
+                        @"articuno",
+                        @"butterfree",
+                        @"charizard",
+                        @"dodrio",
+                        @"doduo",
+                        @"dragonite",
+                        @"farfetch'd",
+                        @"fearow",
+                        @"golbat",
+                        @"gyarados",
+                        @"moltres",
+                        @"pidgeot",
+                        @"pidgeotto",
+                        @"pidgey",
+                        @"scyther",
+                        @"spearow",
+                        @"zapdos",
+                        @"zubat"
+                        
+                        ];
+    
+    NSArray *ghost = @[
+                       @"gastly",
+                       @"gengar",
+                       @"haunter"
+                       ];
+    
+    NSArray* grass =@[
+                      @"bellsprout",
+                      @"bulbasaur",
+                      @"exeggcute",
+                      @"exeggutor",
+                      @"gloom",
+                      @"ivysaur",
+                      @"oddish",
+                      @"paras",
+                      @"parasect",
+                      @"tangela",
+                      @"venusaur",
+                      @"victreebel",
+                      @"vileplume",
+                      @"weepinbell"
+                      ];
+    
+    NSArray *ground = @[
+                        @"cubone",
+                        @"diglett",
+                        @"dugtrio",
+                        @"geodude",
+                        @"golem",
+                        @"graveler",
+                        @"marowak",
+                        @"nidoking",
+                        @"nidoqueen",
+                        @"onix",
+                        @"rhydon",
+                        @"rhyhorn",
+                        @"sandshrew",
+                        @"sandslash"
+                        ];
+    NSArray *ice = @[
+                     @"articuno",
+                     @"cloyster",
+                     @"dewgong",
+                     @"jynx",
+                     @"lapras"
+                     ];
+    
+    NSArray *normal = @[
+                        @"chansey",
+                        @"ditto",
+                        @"dodrio",
+                        @"doduo",
+                        @"eevee",
+                        @"farfetch'd",
+                        @"fearow",
+                        @"jigglypuff",
+                        @"kangaskhan",
+                        @"lickitung",
+                        @"meowth",
+                        @"persian",
+                        @"pidgeot",
+                        @"pidgeotto",
+                        @"pidgey",
+                        @"porygon",
+                        @"ratatta",
+                        @"raticate",
+                        @"snorlax",
+                        @"spearow",
+                        @"tauros",
+                        @"wigglytuff",
+                        @"clefable",
+                        @"clefairy"
+                        
+                        ];
+    
+    NSArray *poison = @[
+                        @"arbok",
+                        @"beedrill",
+                        @"bellsprout",
+                        @"bulbasaur",
+                        @"ekans",
+                        @"gastly",
+                        @"gengar",
+                        @"gloom",
+                        @"golbat",
+                        @"grimer",
+                        @"haunter",
+                        @"ivysaur",
+                        @"kakuna",
+                        @"koffing",
+                        @"muk",
+                        @"nidoking",
+                        @"nidoqueen",
+                        @"nidoran-f",
+                        @"nidoran",
+                        @"nidorina",
+                        @"nidorino",
+                        @"oddish",
+                        @"tentacool",
+                        @"tentacruel",
+                        @"venomoth",
+                        @"venonat",
+                        @"venusaur",
+                        @"victreebel",
+                        @"vileplume",
+                        @"weedle",
+                        @"weepinbell",
+                        @"weezing",
+                        @"zubat"
+                        ];
+    
+    NSArray *psychic = @[
+                         @"abra",
+                         @"alakazam",
+                         @"drowzee",
+                         @"exeggcute",
+                         @"exeggutor",
+                         @"hypno",
+                         @"jynx",
+                         @"kadabra",
+                         @"mew",
+                         @"mewtwo",
+                         @"mr. mime",
+                         @"slowbro",
+                         @"slowpoke"
+                         
+                         
+                         ];
+    
+    NSArray *rock = @[
+                      @"aerodactyl",
+                      @"geodude",
+                      @"golem",
+                      @"graveler",
+                      @"kabuto",
+                      @"kabutops",
+                      @"omanyte",
+                      @"omastar",
+                      @"onix",
+                      @"rhydon",
+                      @"rhyhorn"
+                      
+                      ];
+    
+    
+    NSArray *water = @[
+                       @"blastoise",
+                       @"cloyster",
+                       @"dewgong",
+                       @"goldeen",
+                       @"golduck",
+                       @"gyarados",
+                       @"horsea",
+                       @"kabuto",
+                       @"kabutops",
+                       @"kingler",
+                       @"krabby",
+                       @"lapras",
+                       @"magikarp",
+                       @"omanyte",
+                       @"omastar",
+                       @"poliwag",
+                       @"poliwhirl",
+                       @"poliwrath",
+                       @"psyduck",
+                       @"seadra",
+                       @"seaking",
+                       @"seel",
+                       @"shellder",
+                       @"slowbro",
+                       @"slowpoke",
+                       @"squirtle",
+                       @"starmie",
+                       @"staryu",
+                       @"tentacool",
+                       @"tentacruel",
+                       @"vaporeon",
+                       @"wartortle"
+                       
+                       ];
+    
+    self.pokemonTypes = @{
+                                   
+                                   @"bug" : bug,
+                                   @"dragon" : dragon,
+                                   @"electric" : electric,
+                                   @"fire" : fire,
+                                   @"fighting" : fighting,
+                                   @"flying" : flying,
+                                   @"ghost" : ghost,
+                                   @"grass" : grass,
+                                   @"ground" : ground,
+                                   @"ice" : ice,
+                                   @"normal" : normal,
+                                   @"poison" : poison,
+                                   @"psychic" : psychic,
+                                   @"rock" : rock,
+                                   @"water" : water
+        
+                                   };
     
 }
 
