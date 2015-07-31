aae5b988b353f4c44dc1822f582f6f91c76bceca
Christian <maldonadoguitar@gmail.com>
2015-07-31 19:31:13 -0400

    here you go

---
+++ Pokemon/Pokemon/PokedexTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PokedexTableViewController.h
+//  Pokemon
+//
+//  Created by Christian Maldonado on 7/30/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PokedexTableViewController : UITableViewController
+
+@end
---
+++ Pokemon/Pokemon/infoViewController.h
@@ -0,0 +1,20 @@
+//
+//  infoViewController.h
+//  Pokemon
+//
+//  Created by Christian Maldonado on 7/30/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface infoViewController : UIViewController
+
+@property (nonatomic) NSString *imageName;
+@property (nonatomic) NSString *pokemonName;
+
+
+
+@end
