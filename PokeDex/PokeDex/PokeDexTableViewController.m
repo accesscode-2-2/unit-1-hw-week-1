8bc727503fbf3acbb32b5fe1422ab99e95581fda
elberdev <elberdev@gmail.com>
2015-08-03 19:29:22 -0400

    tweak

--- PokeDex/PokeDex/PokeDexTableViewController.m
+++ PokeDex/PokeDex/PokeDexTableViewController.m
@@ -140,12 +140,11 @@ sectionForSectionIndexTitle:(NSString *)title
     if (self.segmentedSortControl.selectedSegmentIndex == 0) {
         self.pokemonIndex = [[UILocalizedIndexedCollation currentCollation] sectionIndexTitles];
         self.pokemonToDisplay = self.allPokemonAlphabetical;
-        [self.tableView reloadData];
     } else {
         self.pokemonIndex = nil;
         self.pokemonToDisplay = self.allPokemonByType;
-        [self.tableView reloadData];
     }
+    [self.tableView reloadData];
 }
  
  // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -177,41 +176,5 @@ sectionForSectionIndexTitle:(NSString *)title
      vc.pokemonTypes = pokemonTypes;
      vc.pokemonImage = pokemonImage;
  }
-
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
 
 @end
