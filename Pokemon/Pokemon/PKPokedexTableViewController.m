024a0dbe0c69dbf83cadedef65377b759feeb6ef
Jamaal Sedayao <jsedayao@C4Qs-MacBook-Pro.local>
2015-08-03 17:29:46 -0400

    Commit Monday 530pm

--- Pokemon/Pokemon/PKPokedexTableViewController.m
+++ Pokemon/Pokemon/PKPokedexTableViewController.m
@@ -25,7 +25,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.pokemonIndexAtoZ = @[@"A", @"B", @"C", @"D", @"E", @"F", @"G", @"H", @"I", @"J", @"K", @"L", @"M", @"N", @"O", @"P", @"Q", @"R", @"S", @"T", @"U", @"V", @"W", @"X", @"Y", @"Z"];
+    self.pokemonIndexAtoZ = [NSArray arrayWithObjects:@"A", @"B", @"C", @"D", @"E", @"F", @"G", @"H", @"I", @"J", @"K", @"L", @"M", @"N", @"O", @"P", @"Q", @"R", @"S", @"T", @"U", @"V", @"W", @"X", @"Y", @"Z", nil];
     
     
     
